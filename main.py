4e6ada3d883f4c18e942425fca6fbd9716917771
Marcus Luck <marcus.luck@outlook.com>
2016-03-08 19:08:34 -1000

    Add moving of old files to separate folder

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from openpyxl import load_workbook
 import os
+import errno
 
 
 def main():
-    folder = 'input\\'
-    out_folder = 'output\\'
+    folder = 'input/'
+    out_folder = 'output/'
 
     files = os.listdir(folder)
 
@@ -12,14 +13,23 @@ def main():
     out_name = 'Purchase.xlsx'
     output_name = out_folder + out_name
     print(output_name)
-    wb = load_workbook(output_name)
 
+    try:
+        wb = open(output_name)
+    except IOError as e:
+        if e.errno == errno.EACCES:
+            return "some default data"
+            # Not a permission error.
+        raise
+    else:
+        wb = load_workbook(output_name)
 
     # Read each file as a matrix and append the content to a list
     for file in files:
         if file[0] is '~':
             pass
         else:
+            print(file)
             wb_in = load_workbook(folder + file)
             ws = wb.create_sheet()
             ws.title = file[0:30]
@@ -28,12 +38,12 @@ def main():
             for row in ws_in.iter_rows():
                 sb = []
                 for cell in row:
-                    if cell.value != None:
+                    if cell.value is not None:
                         sb.append(str(cell.value))
                     else:
                         sb.append('')
 
-                ws.append( sb )
+                ws.append(sb)
 
     output_name = out_folder + out_name
     print()
@@ -43,7 +53,15 @@ def main():
     except:
         print("Please close Excel file")
 
-
+    # Move read files to input/old
+    old_input = folder + 'old/'
+    if os.path.isdir(old_input) is False:
+        os.makedirs(old_input)
+        for file in files:
+            os.rename(folder + file, old_input + file)
+    else:
+        for file in files:
+            os.rename(folder + file, old_input + file)
 
 if __name__ == '__main__':
     main()
